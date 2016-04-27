c105e5a48b46fb550b8509f3c86ae5575e0aa538
Elena Willen <lnamaso@hotmail.com>
2016-04-27 16:57:31 +0100

    Add NSLog and Label

--- PlayWithGitHub/ViewController.m
+++ PlayWithGitHub/ViewController.m
@@ -17,7 +17,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    NSLog(@"We will learn how to use git with Xcode!");
+    
+    
 }
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
