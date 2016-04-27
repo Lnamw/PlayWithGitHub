7506b5d3d7f6751c88caebbe75a4bbe686223dff
Elena Willen <lnamaso@hotmail.com>
2016-04-27 17:01:54 +0100

    Add MyNewModel Class in the modal branch

---
+++ PlayWithGitHub/MyNewModel.h
@@ -0,0 +1,15 @@
+//
+//  MyNewModel.h
+//  PlayWithGitHub
+//
+//  Created by Elena Maso Willen on 27/04/2016.
+//  Copyright © 2016 Training. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyNewModel : NSObject
+
+@property (strong, nonatomic) NSString *myString;
+
+@end
---
+++ PlayWithGitHub/MyNewModel.m
@@ -0,0 +1,13 @@
+//
+//  MyNewModel.m
+//  PlayWithGitHub
+//
+//  Created by Elena Maso Willen on 27/04/2016.
+//  Copyright © 2016 Training. All rights reserved.
+//
+
+#import "MyNewModel.h"
+
+@implementation MyNewModel
+
+@end
